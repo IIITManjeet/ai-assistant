11c1e0e9a1af07842634c927e92933acedeff68c
IIITManjeet <manjeetpathak2003@gmail.com>
2024-08-07 07:38:44 +0530

    fix:name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,9 @@ const Header = () => {
           src="https://cdn.builder.io/api/v1/image/assets%2F80831ebc592245a9bb7889764d1141b4%2Ffec953d9e0ab46f58524c39d2b982870"
           alt="J&J"
         />
-        <span className="text-white text-3xl font-bold ml-4">SpeechGPT</span>
+        <span className="text-white text-3xl font-bold ml-4">
+          Intelligent UX
+        </span>
       </div>
     </header>
   );
